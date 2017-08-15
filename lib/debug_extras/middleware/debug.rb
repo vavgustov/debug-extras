22ea825d39cd3b76a7bf371bdcc5217c7f8afa93
Vladimir Avgustov <vavgustov@gmail.com>
2017-08-16 00:34:05 +0300

    Refactoring.

--- lib/debug_extras/middleware/debug.rb
+++ lib/debug_extras/middleware/debug.rb
@@ -1,26 +1,24 @@
 module DebugExtras
-  class Middleware
+  class Debug
     def initialize(app)
       @app = app
     end
 
     def call(env)
-      better_errors_fix env
       @app.call env
     rescue StandardError => ex
+      process_exception(ex, env)
+    end
+
+    private
+
+    def process_exception(ex, env)
       if [ex.class, ex.cause.class].map(&:to_s).include? "DebugExtras::DebugData"
         debug_page = DebugPage.new(ex, env["PATH_INFO"])
         [200, { "Content-Type" => "text/html; charset=utf-8" }, [debug_page.render]]
       else
         @app.call env
       end
-    end
-
-    private
-
-    # based on https://github.com/charliesome/better_errors/issues/341
-    def better_errors_fix(env)
-      env["puma.config"].options.user_options.delete(:app) if env.has_key?("puma.config")
     end
   end
 end
--- lib/debug_extras/railtie.rb
+++ lib/debug_extras/railtie.rb
@@ -1,14 +1,29 @@
+require "debug_extras/middleware/debug"
+require "debug_extras/middleware/fast_better_errors"
+
 module DebugExtras
   class Railtie < Rails::Railtie
     initializer "debug_extras.configure_rails_initialization" do
-      if defined? ActionDispatch::DebugExceptions
-        app.middleware.insert_after ActionDispatch::DebugExceptions, DebugExtras::Middleware
-      else
-        app.middleware.use DebugExtras::Middleware
+      if Rails.env.development?
+        insert_middleware(DebugExtras::Debug)
+        insert_middleware(DebugExtras::FastBetterErrors) if better_errors_slow?
       end
     end
 
     private
+
+    def insert_middleware(middleware)
+      if defined? ActionDispatch::DebugExceptions
+        app.middleware.insert_after ActionDispatch::DebugExceptions, middleware
+      else
+        app.middleware.use middleware
+      end
+    end
+
+    def better_errors_slow?
+      return false unless defined? BetterErrors && defined? Puma
+      BetterErrors.binding_of_caller_available && [Rails.version, Puma::Const::PUMA_VERSION].map(&:to_i) == [5, 3]
+    end
 
     def app
       Rails.application
