05224a0b743e0353fbdeea4b3320bf343153dfed
Vladimir Avgustov <vavgustov@gmail.com>
2018-01-24 14:32:47 +0300

    Recover render fix for object names.

--- lib/debug_extras/dumper.rb
+++ lib/debug_extras/dumper.rb
@@ -12,9 +12,24 @@ module DebugExtras
     end
 
     def render
-      message = content_tag(:div, class: @css_class) { @dump.ai(Settings.options).html_safe }
+      message = content_tag(:div, class: @css_class) do
+        render_object_names(@dump.ai(Settings.options)).html_safe
+      end
       yield message if block_given?
       message
+    end
+
+    def render_object_names(dump)
+      output = ""
+      dump.split("<").map.with_index do |v, k|
+        if v.include? ":0x" and not (v.include? "kbd" or v.include? "pre")
+          v.sub!(">", "")
+        else
+          output << "<" unless k.zero?
+        end
+        output << v
+      end
+      output
     end
   end
 end
