39db7583dabf2bb9a2c2ff1173bc8879d75b3d46
Vladimir Avgustov <vavgustov@gmail.com>
2018-01-24 17:21:47 +0300

    Make injector more accurate.

--- lib/debug_extras/injector.rb
+++ lib/debug_extras/injector.rb
@@ -5,27 +5,38 @@ module DebugExtras
     end
 
     def process_response
-      add_messages
-      add_styles
+      if response_is_html?
+        inject_messages
+        inject_styles
+      end
       @response.body
     end
 
     private
 
-    def add_messages
-      return if $debug_extras_messages.blank? || !@response.headers['Content-Type'].include?('html')
+    def response_is_html?
+      return false unless @response.headers['Content-Type'].include?('html')
+      tags = %w[<html> <head> </head <body> </body </html>]
+      tags.each do |tag|
+        return false unless @response.body.include? tag
+      end
+      true
+    end
+
+    def inject_messages
+      return if $debug_extras_messages.blank?
       $debug_extras_messages.map! { |message| DebugExtras::Dumper.new(message, 'debug-wp').render }
       injection = $debug_extras_messages.join('')
-      @response.body = inject_html('<body>') { |html| html[1].prepend(injection) }
+      @response.body = inject_content('<body>') { |html| html[1].prepend(injection) unless html.nil? }
     end
 
-    def add_styles
+    def inject_styles
       return unless $debug_extras_add_styles
       injection = File.read(File.expand_path('../templates/styles.html', __FILE__))
-      @response.body = inject_html('</head>') { |html| html[0] << injection }
+      @response.body = inject_content('</head>') { |html| html[0] << injection }
     end
 
-    def inject_html(tag, &block)
+    def inject_content(tag, &block)
       html = @response.body.split(tag)
       yield html if block_given?
       html.join(tag)
